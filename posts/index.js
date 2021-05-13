d20a7b7e8d232dcbec36c5ac793685f64d75502f
Jason <28811675+xynoipse@users.noreply.github.com>
2021-05-13 16:34:58 +0800

    posts service

---
+++ posts/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(express.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.json(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).json(posts[id]);
+});
+
+app.listen(4000, () => console.log('Listening on port 4000'));
